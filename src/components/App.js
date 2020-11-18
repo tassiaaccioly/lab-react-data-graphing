d053737eccfa210812450fa222766869b71c4577
rschpdr <pedro.resch@gmail.com>
2020-11-18 15:39:12 -0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,7 @@
 import React from "react";
-import Graph from "./Graph";
 
 function App() {
-  return (
-    <div>
-      <Graph id="bpiGraph" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
